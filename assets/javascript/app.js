7a07e6637b809f75a9b7f454cae017272262a0f5
Pearl Jean <perlegirl@protonmail.com>
2019-12-07 17:04:57 -0500

    added final funcitonalities: pictures displayed at the show answer page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,28 +6,28 @@ $(document).ready(function () {
             question: "which is not a canine breed?",
             answerChoice: ["cattle dog", "corndog", "shepherd dog", "bulldog"],
             theA: "corndog",
-            img: "../images/corndog.jpeg"
+            img: '<img id="pix" src="C:\\Users\\Pearl\\Desktop\\corndog.jpeg" >'
         },
 
         {
             question: "This animal eats fruits and lives inside trees?",
             answerChoice: ["great horned owls", "sumatran rhinos", "japanese flying dwarf squirrel", "saiga antelopes"],
             theA: "japanese flying dwarf squirrel",
-            img: "../images/warf.jpeg"
+            img: '<img id="pix" src="C:\\Users\\Pearl\\Desktop\\warf.jpeg" >'
         },
 
         {
             question: "In Moby Dick, the main character is: ",
             answerChoice: ["sperm whale", "narwhal", "great white", "killer whale"],
             theA: "sperm whale",
-            img: "../images/moby.jpeg"
+            img: '<img id="pix"  src="C:\\Users\\Pearl\\Desktop\\moby.jpeg" >'
         },
 
         {
             question: "what is another name for flying foxes?",
             answerChoice: ["cats", "fruit flies", "hamsters", "fruit bats"],
             theA: "fruit bats",
-            img: "../images/batty.jpeg"
+            img: '<img id="pix" src="C:\\Users\\Pearl\\Desktop\\batty.jpeg" >'
         },]
 
 
@@ -49,11 +49,11 @@ $(document).ready(function () {
         timeLeft--;
         if (timeLeft == 5) {
             showans();
-                unanswered++;
+            unanswered++;
             console.log("unanswered");
         }
-        if (timeLeft == -1) { 
-            if(questionNumber == 3 ) {
+        if (timeLeft == -1) {
+            if (questionNumber == 3) {
                 console.log("THE END OF QUESTIONS");
                 $("#answerPg").hide();
                 $("#questionPg").hide();
@@ -64,19 +64,22 @@ $(document).ready(function () {
                 $("#restartButt").html("Restart Game!");
                 // clearInterval(timer);
                 $("#end").show();
-            }  else { 
+            } else {
                 $("#answerPg").hide();
-                timeLeft = 20;      
+                timeLeft = 20;
                 displayQ();
-                }
-        }       
+            }
+        }
     }
 
-var showans = function() {
-    $("#questionPg").hide();
-    $("#answerPg").show();
-    $("#correctA").html("The Correct Answer is " + vals[questionNumber].theA  );
-}
+    var showans = function () {
+        $('#pics').html('');
+        $('#pics').append(vals[questionNumber].img);
+
+        $("#questionPg").hide();
+        $("#answerPg").show();
+        $("#correctA").html("Correct Answer is: " + vals[questionNumber].theA);
+    }
     function displayQ() {
         questionNumber++;
         $("#questionPg").show();
@@ -86,37 +89,38 @@ var showans = function() {
         }
     }
 
-$(".answerList").on("click", function() {
-    timeLeft = 2;
-    showans();
-    var userans = $(this).text();
-    var correctans = vals[questionNumber].theA;
+    $(".answerList").on("click", function () {
+        timeLeft = 5;
+        showans();
+        var userans = $(this).text();
+        var correctans = vals[questionNumber].theA;
 
-    if(  userans == correctans ) {
-        console.log("correct");
-        correct ++;
-    } else  {console.log("incorrect");
+        if (userans == correctans) {
+            console.log("correct");
+            correct++;
+        } else {
+            console.log("incorrect");
             incorrect++;
-}
-    showans();     
+        }
+        // showans();
 
-} );
+    });
 
 
-$("#restartButt").on("click", function() {
-    
-    $("#restartButt").hide();
-    $("#questionPg").show();
-    $("#end").hide();
-    $("#timer").show();
-    setInterval(timer, 1000);
-     questionNumber = -1;
-    timeLeft = 20;
-    displayQ();     
-    correct = 0;
-    incorrect = 0;
-    unanswered = 0;
-} );
+    $("#restartButt").on("click", function () {
+
+        $("#restartButt").hide();
+        $("#questionPg").show();
+        $("#end").hide();
+        $("#timer").show();
+        setInterval(timer, 1000);
+        questionNumber = -1;
+        timeLeft = 20;
+        displayQ();
+        correct = 0;
+        incorrect = 0;
+        unanswered = 0;
+    });
 
 });
 
